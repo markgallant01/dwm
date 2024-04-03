e5e1a9f0b7a3a1fa5cca8f7727d645878ec60407
markgallant01 <markgallant01@gmail.com>
2024-04-03 03:13:14 -0400

    change quit keybind to mod + r

--- config.h
+++ config.h
@@ -112,7 +112,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
   /* my keybinds start here */
   { 0, XF86XK_MonBrightnessUp, spawn, {.v = upbright } },
   { 0, XF86XK_MonBrightnessUp, spawn, SHCMD("~/.dotfiles/scripts/update_statusbar.sh") },
