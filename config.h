362925cb90ce952e968338ab1092cdf091c4653f
markgallant01 <markgallant01@gmail.com>
2024-07-16 21:01:52 -0400

    increase pixel border size

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
