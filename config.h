0248d4d729fc724506630ddb396039a79769e00f
markgallant01 <markgallant01@gmail.com>
2025-08-12 19:12:17 -0400

    update dmenu font

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Liberation Mono:size=12",
                                         "Symbols Nerd Font:size=16"};
-static const char dmenufont[]       = "Noto Sans Mono:size=11";
+static const char dmenufont[]       = "Liberation Mono:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
