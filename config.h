13e087c6410222ba395141909419d87021508750
markgallant01 <markgallant01@gmail.com>
2025-08-13 01:02:29 -0400

    apply statuspadding patch

--- config.h
+++ config.h
@@ -11,6 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 7;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "Liberation Mono:size=12",
                                         "Symbols Nerd Font:size=16"};
 static const char dmenufont[]       = "Liberation Mono:size=11";
