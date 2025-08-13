0983bf536dfb7050dea7cd65eb259c4a5998f7f9
markgallant01 <markgallant01@gmail.com>
2025-08-13 00:31:22 -0400

    add fancy icons and a tag rule for firefox

--- config.h
+++ config.h
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "󰈹", "󰧮", "", "", "", "", "", "󰗹" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -35,7 +35,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox-esr",  NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
@@ -46,8 +46,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[t]",      tile },    /* first entry is default */
-	{ "[f]",      NULL },    /* no layout function means floating behavior */
+	{ "󰰤",      tile },    /* first entry is default */
+	{ "󰯺",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
