43414f3d57b49f7a7a69a6797c644dfbcae21393
markgallant01 <markgallant01@gmail.com>
2025-09-10 12:55:55 -0400

    update zenity rule to make it floating like the other steam updater window

--- config.h
+++ config.h
@@ -73,7 +73,7 @@ static const Rule rules[] = {
     { "discord",         NULL,       NULL,       1 << 3,     0,          -1 },
     { "steam",           NULL,       NULL,       1 << 4,     0,          -1 },
     { NULL,              NULL,       "Steam",    1 << 4,     0,          -1 },
-    { "zenity",          NULL,       NULL,       1 << 4,     0,          -1 },
+    { "zenity",          NULL,       NULL,       1 << 4,     1,          -1 },
     { "blueman-manager", NULL,       NULL,       0,          1,          -1 },
     { "Blueman-manager", NULL,       NULL,       0,          1,          -1 },
     { "pavucontrol",     NULL,       NULL,       0,          1,          -1 },
